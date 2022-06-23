e9c5095399980fbdd957f54f24ce13bd1b4e6e77
khalil <khalillakhdharatc@gmail.com>
2022-06-23 15:16:36 +0100

    secure

--- src/main/java/com/doranco/security/SecrureTasksApplication.java
+++ src/main/java/com/doranco/security/SecrureTasksApplication.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.doranco.security.model.AppRole;
+import com.doranco.security.model.AppUser;
 import com.doranco.security.model.Task;
+import com.doranco.security.service.AccountService;
 import com.doranco.security.service.TaskService;
 
 @SpringBootApplication
@@ -13,12 +18,20 @@ public class SecrureTasksApplication implements CommandLineRunner {
 
 	@Autowired
 	TaskService taskService;
-	
+	@Autowired
+	AccountService accountService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SecrureTasksApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder getBCPE()
+	{
+	return new BCryptPasswordEncoder();	
+	}
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
@@ -29,6 +42,15 @@ public class SecrureTasksApplication implements CommandLineRunner {
 			// t = le task qui vient de taskService.getTasks() (déclaration et affectation)
 			// aprés ->{ } on écrit ce qu'on veut faire
 			System.out.println(t.toString());
+			
+			accountService.saveUser(new AppUser("admin", "1234",1));
+			accountService.saveUser(new AppUser("user", "1234",1));
+			accountService.saveRole(new AppRole("ADMIN"));
+			accountService.saveRole(new AppRole("USER"));
+			accountService.addRoleToUser("admin", "ADMIN");
+			accountService.addRoleToUser("admin", "USER");
+			accountService.addRoleToUser("user", "USER");
+			
 		}
 		);
 		
